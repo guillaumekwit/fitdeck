30fb8d0f3d8271612ef0dbe2409789822c8c387d
guillaumekwit <55895280+guillaumekwit@users.noreply.github.com>
2022-08-01 02:26:56 -0400

    .

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080/api/v1/exercise",
+  baseURL: "https://fitdeck.herokuapp.com",
   headers: {
     "Content-type": "application/json"
   }
