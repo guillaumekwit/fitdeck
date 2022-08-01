6789250a2256c75c97943596e4a090181b99b9aa
guillaumekwit <55895280+guillaumekwit@users.noreply.github.com>
2022-08-01 02:42:29 -0400

    This commit is what is live currently

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://fitdeck.herokuapp.com",
+  baseURL: "https://fitdeck.herokuapp.com/api/v1/exercise",
   headers: {
     "Content-type": "application/json"
   }
